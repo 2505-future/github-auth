4f8211e731426bda43bb02feeee76ea8562e0189
ogatakatsuya <katsuya151225@icloud.com>
2025-05-10 03:23:04 +0900

    feat: client

---
+++ app/utils/getBrowserClient.ts
@@ -0,0 +1,17 @@
+import { createBrowserClient } from '@supabase/ssr'
+import { SupabaseClient } from '@supabase/supabase-js'
+
+let supabase: SupabaseClient | null = null
+
+type SupabaseClientProps = {
+  url: string
+  key: string
+}
+
+export function getSupabaseClient(props: SupabaseClientProps): SupabaseClient{
+  if (!supabase) {
+    supabase = createBrowserClient(props.url, props.key)
+  }
+
+  return supabase
+}
---
+++ app/utils/getServerClient.ts
@@ -0,0 +1,27 @@
+import { createServerClient, parseCookieHeader, serializeCookieHeader } from '@supabase/ssr'
+
+export function createSupabaseServerClient(request: Request) {
+  const cookies = parseCookieHeader(request.headers.get("Cookie") ?? "");
+  const headers = new Headers();
+
+  const supabase = createServerClient(
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return parseCookieHeader(request.headers.get('Cookie') ?? '').filter(
+            (cookie): cookie is { name: string; value: string } => cookie.value !== undefined
+          );
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            headers.append('Set-Cookie', serializeCookieHeader(name, value, options))
+          )
+        },
+      },
+    }
+  );
+
+  return { supabase, headers }
+}
