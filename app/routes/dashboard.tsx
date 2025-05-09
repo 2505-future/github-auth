edf90e2c85f86cb6003b1bcca3a834abde618c94
ogatakatsuya <katsuya151225@icloud.com>
2025-05-10 03:42:29 +0900

    fix: output log on clien side

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,4 +1,5 @@
 import { type LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import { getSessionFromCode } from "~/utils/auth";
 
 export async function loader({ request }: LoaderFunctionArgs) {
@@ -7,14 +8,15 @@ export async function loader({ request }: LoaderFunctionArgs) {
 
 	if (code) {
 		const { data } = await getSessionFromCode(request, code);
-		console.log("data", data);
-		return null;
+		return data;
 	}
 
 	return redirect("/auth/auth-code-error");
 }
 
 export default function AuthCode() {
+	const data = useLoaderData();
+	console.log("Authentication Data:", data);
 	return (
 		<div>
 			<h1>Auth Code</h1>
@@ -22,3 +24,4 @@ export default function AuthCode() {
 		</div>
 	);
 }
+
